01533cdfc8752df33afef17e336e1cd4af35acf8
Showrin <showrinece15@gmail.com>
2022-08-16 05:38:00 +0600

    :heavy_plus_sign: :wrench: Add some dependencies and configure rollup and babel

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import sass from "rollup-plugin-sass";
+import resolveNodeModules from "@rollup/plugin-node-resolve";
+import babel from "@rollup/plugin-babel";
+import pkg from "./package.json";
+
+export default {
+  input: "src/index.jsx",
+  output: [
+    {
+      file: pkg.main,
+      format: "cjs",
+      exports: "named",
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    sass({ insert: true }),
+    resolveNodeModules(),
+    babel({
+      babelrc: true,
+      babelHelpers: "runtime",
+      exclude: "node_modules/**",
+    }),
+  ],
+  external: ["react", "react-dom", "@babel/runtime"],
+};
