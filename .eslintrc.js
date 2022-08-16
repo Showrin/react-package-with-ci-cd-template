19ed289ec5fe7abf081937c0e9892bbbc38f00f5
Showrin <showrinece15@gmail.com>
2022-08-16 06:01:19 +0600

    :heavy_plus_sign: :wrench: Add some dependencies and configure eslint

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["plugin:react/recommended", "airbnb"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "react/jsx-key": 2,
+    "react/function-component-definition": [
+      2,
+      {
+        namedComponents: "arrow-function",
+        unnamedComponents: "arrow-function",
+      },
+    ],
+    "import/extensions": [
+      1,
+      "always",
+      { ignorePackages: true, pattern: { js: "never", jsx: "never" } },
+    ],
+  },
+};
