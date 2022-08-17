0032526954c82965c982b794c7bebce6a6520f2c
Showrin <showrinece15@gmail.com>
2022-08-17 07:50:03 +0600

    :construction_worker: Generate cobertura format coverage report from jest

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ["src/**/*.{js,jsx}"],
   coverageDirectory: "coverage",
+  coverageReporters: ["text", "cobertura"],
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
 };
